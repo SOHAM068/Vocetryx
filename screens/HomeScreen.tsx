1e0dc892fcf86ff2c9802049afa2b5c225b3fa8c
SOHAM068 <sg3533@srmist.edu.in>
2024-11-06 07:44:03 +0530

    Some changes

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -842,7 +842,7 @@ const styles = StyleSheet.create({
       width: 0,
       height: 2,
     },
-    shadowOpacity: 0.25,
+    shadowOpacity: 0.26,
     shadowRadius: 3.84,
     elevation: 5,
   },
